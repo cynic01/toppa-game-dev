2ce06f24fcdf76a654fcb61c79e043dc15fcbd2b
Tianye Meng <66190734+Dalton-M@users.noreply.github.com>
2022-10-30 01:35:33 -0700

    Updated Movements and Camera
    Player now always goes forward to the direction of the camera. Rotating the camera now also rotates the player.

--- toppa/Assets/Scripts/BasicMovements.cs
+++ toppa/Assets/Scripts/BasicMovements.cs
@@ -7,15 +7,21 @@ public class BasicMovements :  MonoBehaviour
 {
     [SerializeField]
     private float m_speed;
+
+    [SerializeField]
+    private float MouseSensitivity;
+
+    [SerializeField]
+    private GameObject Camera;
     
     #region Private Variables
-    private Vector3 Velocity;
+    private Vector2 Velocity;
     #endregion
     
     #region Cached Components
     private Rigidbody Rb;
     #endregion
-    
+    private float heading = 0f;
 
     void Awake () {
         Debug.Log("Awaked");
@@ -33,15 +39,37 @@ public class BasicMovements :  MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
+        heading += Input.GetAxis("Mouse X")*Time.fixedDeltaTime;
+        transform.rotation = Quaternion.Euler(0, heading, 0);
+        
         float p_verticalSpeedController = Input.GetAxis("Vertical");
         float p_horizontalSpeedController = Input.GetAxis("Horizontal");
-        Velocity.Set(p_horizontalSpeedController, 0f, p_verticalSpeedController);
+        Velocity.Set(p_horizontalSpeedController, p_verticalSpeedController);
+        Velocity = Vector2.ClampMagnitude(Velocity, 1);
+        
+        Vector3 camF = Camera.transform.forward;
+        Vector3 camR = Camera.transform.right;
+        
+        camF.y = 0;
+        camR.y = 0;
+        camF = camF.normalized;
+        camR = camR.normalized;
+        transform.position += camF * p_verticalSpeedController/10 + camR * p_horizontalSpeedController/10;
+        
+        Vector3 MovementDirection = camF;
+        
+        MovementDirection = MovementDirection.normalized;
+        if (MovementDirection != Vector3.zero) {
+            transform.forward = MovementDirection;
+        }
+        //transform.position += new Vector3(Velocity.x, 0, Velocity.y) * Time.fixedDeltaTime*5;
+        
     }
 
     void FixedUpdate () {
         // Debug.Log("m_speed is" + m_speed + " and DeltaTime is" + Time.fixedDeltaTime);
-        // Debug.Log(Velocity.magnitude + " is the magnitude of velocity"); 
+        // Debug.Log(Velocity.magnitude + " is the magnitude of velocity");
+        //Rb.velocity = Velocity * m_speed * Time.fixedDeltaTime * Velocity.magnitude;     
         
-        Rb.velocity = Velocity * m_speed * Time.fixedDeltaTime * Velocity.magnitude;         
     }
 }
