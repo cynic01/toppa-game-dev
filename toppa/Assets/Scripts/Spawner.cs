98166c507b2a8e3e1a1581fda40fb9086867bd73
Edison Chen <zx.chen@berkeley.edu>
2022-10-30 18:37:39 -0700

    decorate

--- toppa/Assets/Scripts/Spawner.cs
+++ toppa/Assets/Scripts/Spawner.cs
@@ -24,8 +24,8 @@ public class Spawner : MonoBehaviour
         CreateTime -= Time.deltaTime;
         // Spawn an object in the given space
         if (CreateTime <= 0) {
-            GameObject go2 = Instantiate(go, new Vector3(Random.Range(0f, 10f), -1, Random.Range(0f, 10f)), Quaternion.identity);
-            CreateTime = Random.Range(2, 5);
+            GameObject go2 = Instantiate(go, new Vector3(Random.Range(-20f, 50f), -1, Random.Range(-20f, 50f)), Quaternion.identity);
+            CreateTime = Random.Range(1, 5);
             enemyCount--;
         }
     }
