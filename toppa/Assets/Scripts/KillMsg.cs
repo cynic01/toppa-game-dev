4a8dc7f450ab6da5c6380e68037529f9e2f96b15
Lai Wei <tonywei2002@berkeley.edu>
2022-11-20 03:44:12 +0800

    add killUI

---
+++ toppa/Assets/Scripts/KillMsg.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillMsg : MonoBehaviour
+{
+    public Text hintText;
+    private string hintmsg = "Slained: ";
+    private float nextShowTime;
+    
+    void Start()
+    {
+        hintText.enabled = false;
+        float nextShowTime = 0f;
+    }
+
+    // Update is called once per frame
+    public void ShowMsg(string SlainedName) 
+    {
+        hintmsg += SlainedName;
+        DisplayMsg();
+    }
+
+    private void DisplayMsg()
+    {
+        hintText.enabled = true;
+        hintText.text = hintmsg;
+        hintText.CrossFadeAlpha(1, 0.2f, false);
+        
+        Invoke("HideMsg", 3);
+    }
+
+    private void HideMsg()
+    {
+        hintText.CrossFadeAlpha(0, 0.2f, false);
+        //hintText.enabled = false;
+        hintmsg = "Slained: ";
+    }
+
+    void FixedUpdate()
+    {
+        if (nextShowTime < Time.time)
+        {
+            ShowMsg("Cube");
+            nextShowTime = Time.time + 10f;
+        }
+        
+    }
+}
